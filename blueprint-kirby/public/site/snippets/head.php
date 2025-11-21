d0eac3ee9d8e05b7c2fd241ea3ef29b6f566ce4e
Tobias Wolf <hello@tobiaswolf.me>
2025-11-21 15:57:43 +0100

    Fix og:image meta tag
    Signed-off-by: Tobias Wolf <hello@tobiaswolf.me>

--- blueprint-kirby/public/site/snippets/head.php
+++ blueprint-kirby/public/site/snippets/head.php
@@ -26,7 +26,7 @@ use Kirby\Cms\Page;
 	<meta property="og:title" content="<?= $page->title()->esc('attr') ?>">
 	<meta property="og:site_name" content="<?= $site->title()->esc('attr') ?>">
 	<?php if ($ogImage = $page->metaImage()->toFile()): ?>
-		<meta property="og:image" content="<?= $ogImage->thumb('og-image') ?>">
+		<meta property="og:image" content="<?= $ogImage->thumb('og-image')->url() ?>">
 		<meta property="og:image:alt" content="<?= $ogImage->alt()->esc('attr') ?>">
 		<meta property="og:image:width" content="1200">
 		<meta property="og:image:height" content="630">
