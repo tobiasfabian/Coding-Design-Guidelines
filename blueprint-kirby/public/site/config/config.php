b5ebe20028c0b71441db2f01f0e08548267977c9
Tobias Wolf <hello@tobiaswolf.me>
2025-11-21 18:34:32 +0100

    Add slug option
    Signed-off-by: Tobias Wolf <hello@tobiaswolf.me>

--- blueprint-kirby/public/site/config/config.php
+++ blueprint-kirby/public/site/config/config.php
@@ -3,6 +3,7 @@
 return [
 	// Kirby options
 	'locale' => 'de_DE.utf-8',
+	'slugs' => 'de',
 	'routes' => require __DIR__ . '/options/routes.php',
 	'thumbs' => require __DIR__ . '/options/thumbs.php',
 	'updates' => [
